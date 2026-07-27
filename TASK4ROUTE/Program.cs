fa2b291b93d295f8256138e7d5d355e6d3e24963
ibrahim mahmoud elghandour <ibrahimelghandour51@gmail.com>
2026-07-27 19:18:10 +0300

    Task 3: Method with no parameters

--- TASK4ROUTE/Program.cs
+++ TASK4ROUTE/Program.cs
@@ -9,6 +9,19 @@
             Console.WriteLine($"Task 1: {prices[1]}");
             Console.WriteLine();
             #endregion
+            #region Task 2
+            int[,] shelfCopies = { { 3, 5 }, { 1, 4 } };
+            Console.WriteLine($"Task 2: {shelfCopies[1, 0]}");
+            Console.WriteLine();
+            #endregion
+            #region Task 3
+            PrintWelcomeMessage();
+            Console.WriteLine();
+            #endregion
+        }
+        static void PrintWelcomeMessage()
+        {
+            Console.WriteLine("Task 3: Welcome to the Library!");
         }
     }
 }
